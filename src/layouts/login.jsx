b24ec6ea70fa3357996decfa3453251f2a8195b8
ine-nk <its.nvkz@gmail.com>
2022-01-16 23:43:03 +0700

    changed in module Login 9-2

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (<h2> Login </h2>)
+  const [email, setEmail] = useState('')
+
+  const handleChange = (e) => {
+    setEmail(e.target.value)
+    console.log(e.target.name)
+  }
+  return <form className='m-3' action="">
+    <div className='mb-3'>
+      <label className="form-label" htmlFor="email">Email</label>
+      <input className="form-control" type="text"
+        id='email'
+        name='email'
+        value={ email }
+        onChange={ handleChange } /></div>
+    <div className='mb-3'>
+      <label className="form-label"
+        htmlFor="password" >Password</label>
+      <input className="form-control"
+        type="password"
+        name='password'
+        id='passsword'
+        onChange={ handleChange } /></div>
+  </form>
 }
 
 export default Login
